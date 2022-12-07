e754b01167c7f08cdb30974b0493876c07c5e66d
Anton <antonkraschenko@ukr.net>
2022-12-07 12:06:03 +0200

    added a hiddenActiv function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 'use strick';
 
 
+window.addEventListener('DOMContentLoaded', () => {
 
+    const tabsWrapper = document.querySelector('.tabheader__items');
+    const tabs = tabsWrapper.querySelectorAll('.tabheader__item');
+    const tabsContent = document.querySelectorAll('.tabcontent');
+
+
+    function hiddenActiv() {
+
+        tabsContent.forEach( item => {
+            item.style.display = 'none';
+        });
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    hiddenActiv();
+});
 
 
