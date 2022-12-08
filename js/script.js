e240881a3eee6601127c20c4871acc747b43f60e
Anton <antonkraschenko@ukr.net>
2022-12-08 22:03:21 +0200

    added modal window

--- js/script.js
+++ js/script.js
@@ -116,98 +116,48 @@ window.addEventListener('DOMContentLoaded', () => {
     setClock('.timer', deadline);
 
 
+    // Make modal window 
+    const btnModal = document.querySelectorAll('[data-modal]');
+    const btnClose = document.querySelector('[data-close]');
+    const modal = document.querySelector('.modal');
 
 
-    
+    // btnClose.addEventListener('click', (event) => {
+    //     modal.style.display = 'none';
+    // });
+
+    btnModal.forEach(btn => {
+
+        btn.addEventListener('click', (event) => {
+            modal.classList.add('show');
+            modal.classList.remove('hidden');
+            document.body.style.overflow = 'hidden';
+        });
+    });
 
 
+    function closeModal() {
+        modal.classList.remove('show');
+        modal.classList.add('hidden');
+        document.body.style.overflow = '';
+    }
 
+    // close click btn
+    btnClose.addEventListener('click', closeModal);
 
+    // close click modal
+    modal.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
 
+    // close click Escape
+    document.addEventListener('keydown', (event) => {
+        if (event.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // const deadline = '2022-12-31';
-
-    // function getTimeRemaining(endtime) {
-    //     const dateNow = new Date();
-    //     let t = Date.parse(endtime) - Date.parse(dateNow);
-
-    //     const days = Math.floor(t / (1000 * 60 * 60 * 24));
-    //     const hours = Math.floor(t / (1000 * 60 * 60) % 24);
-    //     const minutes = Math.floor(t / (1000 * 60) % 60);
-    //     const seconds = Math.floor((t / 1000) % 60);
-
-    //     return {
-    //         'total': t,
-    //         'days': days,
-    //         'hours': hours,
-    //         'minutes': minutes,
-    //         'seconds': seconds
-    //     };
-    // }
-
-    // function setClock(selector, endtime) {
-    //     const timer = document.querySelector(selector);
-    //     const days = timer.querySelector('#days');
-    //     const hours = timer.querySelector('#hours');
-    //     const minutes = timer.querySelector('#minutes');
-    //     const seconds = timer.querySelector('#seconds');
-        
-    //     const timeInterval = setInterval(updateClock, 1000);
-        
-    //     updateClock();
-
-    //     function updateClock() {
-    //         const t = getTimeRemaining(endtime);
-
-    //         function getZero(num) {
-                
-    //             if( num >= 0 && num < 10) {
-    //                 return '0' + num;
-    //             } else {
-    //                 return num;
-    //             }
-
-    //         }
-
-    //         days.innerHTML = t.days;
-    //         hours.innerHTML = getZero(t.hours);
-    //         minutes.innerHTML = t.minutes;
-    //         seconds.innerHTML = t.seconds;
-
-    //         if (t.total <= 0) {
-    //             clearInterval(timeInterval);
-    //         } 
-    //     }
-
-    // }
-
-    // setClock('.timer', deadline);
-    
-
+        }
+    });
 });
 
