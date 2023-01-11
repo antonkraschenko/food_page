4ad75dce0d88eb88ec9bfcddb469e2084fa2c9f9
Anton <antonkraschenko@ukr.net>
2023-01-11 21:48:17 +0200

    added a fetch for cords from the data-base

--- js/script.js
+++ js/script.js
@@ -219,36 +219,40 @@ window.addEventListener('DOMContentLoaded', () => {
             this.parent.append(div);
         }
     }
-/* jshint ignore:start */
-    new Card(
-        "img/tabs/vegy.jpg",
-        'vegy',
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        '.menu__field .container',
-        'menu__item'
-    ).render();
-    
-    new Card(
-        "img/tabs/elite.jpg",
-        'elite',
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        20,
-        '.menu__field .container',
-        'menu__item'
-    ).render();
 
-    new Card(
-        "img/tabs/post.jpg",
-        'post',
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        16,
-        '.menu__field .container',
-        'menu__item'
-    ).render();
+       const getResource = async (url) => {
+            const res = await fetch(url);
+
+            if (!res.ok) {
+                // отлавливаем ошибку, так как fetch может ее пропустить
+                throw new Error(`Could not fetch ${url}, status: ${res.status}`);   
+            }
+
+            return await res.json();
+       };
+       getResource('http://localhost:3000/menu')
+       .then(data => {
+            // перебераем каждый елемент масива и делаем деструктуризацию данных
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new Card(img, altimg, title, descr, price, '.menu__field .container').render();
+            });
+       });
+
+
+
+/* jshint ignore:start */
+
+    // старый вариант ревлизации карточек на сайте, выше новы с получением данных с сервера 
+
+    // new Card(
+    //     "img/tabs/vegy.jpg",
+    //     'vegy',
+    //     'Меню "Фитнес"',
+    //     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    //     9,
+    //     '.menu__field .container',
+    //     'menu__item'
+    // ).render();
 
 /* jshint ignore:end */
 
